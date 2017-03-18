70f49b22e902fdefe6d71497889341eb390a719b
Rushy Panchal <panchal.rushy@gmail.com>
2017-03-18 16:48:44 -0400

    Configure Django to use Postgres

--- coursevision/settings.py
+++ coursevision/settings.py
@@ -73,11 +73,9 @@ WSGI_APPLICATION = 'coursevision.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
