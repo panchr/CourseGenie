bd522795791bc72352b3d40c1f8561f10af6042a
Kathy Fan <kathy.z.fan@gmail.com>
2017-04-04 20:44:55 -0400

    edited models

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
+# core/views.py
+# CourseGenie
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from core.models import *
 
 # Create your views here.
 class IndexView(LoginRequiredMixin, TemplateView):
