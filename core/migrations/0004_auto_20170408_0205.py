06f1a6406482b68b340ac17787e875c59d15e30b
Rushy Panchal <panchal.rushy@gmail.com>
2017-04-07 22:22:07 -0400

    Remove Kathy's invalid "B.S.E." primary key default

--- core/migrations/0004_auto_20170408_0205.py
+++ core/migrations/0004_auto_20170408_0205.py
@@ -34,7 +34,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='calendar',
             name='degree',
-            field=models.ForeignKey(default='B.S.E.', on_delete=django.db.models.deletion.CASCADE, to='core.Degree'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='core.Degree'),
             preserve_default=False,
         ),
         migrations.AddField(
