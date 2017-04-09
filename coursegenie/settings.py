227381bb19c677805a4930cee9a525bf813b3a7b
ahargil <anya.hargil@gmail.com>
2017-04-09 12:33:25 -0400

    created api branch wih view.py, serializers.py, urls.py

--- coursegenie/settings.py
+++ coursegenie/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAdminUser',
     ],
     'PAGE_SIZE': 10
 }
