2d7a60f21388831fb09cbcbea43c62e9840db33c
Kathy Fan <kathy.z.fan@gmail.com>
2017-04-24 18:55:35 -0400

    green messages

--- core/static/react/core/components/RecommendationDisplay.jsx
+++ core/static/react/core/components/RecommendationDisplay.jsx
@@ -15,7 +15,7 @@ function RecommendationDisplay(props) {
 			<div>{props.course.short_name} ({props.course.term_display})</div>
 			<div>{props.course.name}</div>
 			<div style={{color: 'CadetBlue'}}>Reason:</div>
-			<div>{props.reason_list.map((r) => <div><span key={Math.random()}>{r}</span><br/></div>)}</div>
+			<div style={{color: 'CadetBlue'}}>{props.reason_list.map((r) => <div><span key={Math.random()}>{r}</span><br/></div>)}</div>
 		</div>
 	</div>);
 	}
